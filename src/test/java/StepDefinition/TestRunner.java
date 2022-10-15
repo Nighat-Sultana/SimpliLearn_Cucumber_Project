26cdf4c43a2dd773369cd1fe3c384c2686359e01
Nighat-Sultana <sultana.nighat73@gmail.com>
2022-10-15 01:58:26 -0400

    Cucumber First commit

---
+++ src/test/java/StepDefinition/TestRunner.java
@@ -0,0 +1,26 @@
+package StepDefinition;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		plugin = {"pretty","html:target/html-cucumber", "json:target/cucumber.json"},
+		features ="src/test/java/features",
+		glue = "StepDefinition",
+		tags = {"@sanity"},
+		monochrome = true
+			)
+
+public class TestRunner {
+
+	
+	
+	
+	
+	
+
+}
